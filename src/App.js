9515b66c87a96fb37b32fe74488a895e63c961f8
iiimomoniii <natdanai_thaiyala@live.com>
2023-08-29 17:36:24 +0700

    use hook for count when click

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default () => {
   let var1 = 0;
-
+  //count = variable
+  //setCount = function for count variable
+  //0 = initial variable
+  const [count, setCount] = useState(0)
   return (
     <div>
       JSX
-      <h1>count {var1}</h1>
+      <h1>Count {count}</h1>
       <button
         onClick={() => {
           var1 = var1 + 1;
+          //action
+          setCount(count+1);
           console.log(var1);
         }}
       >
