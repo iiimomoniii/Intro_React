ae47f737f5a91dbe99766c23b45da992c0a90bc6
iiimomoniii <natdanai_thaiyala@live.com>
2023-08-29 17:29:15 +0700

    prepair function for count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default () => {
+  let var1 = 0;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      JSX
+      <h1>count {var1}</h1>
+      <button
+        onClick={() => {
+          var1 = var1 + 1;
+          console.log(var1);
+        }}
+      >
+        Add
+      </button>
     </div>
   );
-}
-
-export default App;
+};
