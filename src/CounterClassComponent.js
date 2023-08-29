951dbc56c1ff9fcc38ddd4680f5c5796861ab648
iiimomoniii <natdanai_thaiyala@live.com>
2023-08-29 18:37:19 +0700

    react hook for setCount

--- src/CounterClassComponent.js
+++ src/CounterClassComponent.js
@@ -5,7 +5,6 @@ export default class CounterClassComponent extends Component {
     //update value in setState function
     constructor(props) {
       super(props)
-    
       this.state = {
          count:0
       }
@@ -16,7 +15,6 @@ export default class CounterClassComponent extends Component {
       <div style={{ backgroundColor: "red"}}>
         <h1>Counter Class Component</h1>
         <h3>Counter : {this.state.count}</h3>
-        
         <button onClick={()=>{
             this.setState({count:this.state.count +1 })
         }}>Add</button>
--- src/CounterFunctionalComponent.js
+++ src/CounterFunctionalComponent.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React,  {useState} from 'react'
 
 export default function CounterFunctionalComponent() {
+    //use React Hook
+    const [count, setCount] = useState(0);
   return (
-    <div>Counter Functional Component</div>
+    <div style={{backgroundColor: "yellow"}}>
+        <h1>Counter Class Component</h1>
+        <h3>Counter : {count}</h3>
+        <button onClick={()=>
+            setCount(count+1)
+        }>Add</button>
+    </div>
   )
 }
