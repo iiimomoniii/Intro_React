ae4d20b897e2e8791e030c23d04f0864237ae76b
iiimomoniii <natdanai_thaiyala@live.com>
2023-08-29 18:24:41 +0700

    how to setState in counter class component

--- src/CounterClassComponent.js
+++ src/CounterClassComponent.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 export default class CounterClassComponent extends Component {
+
+    //update value in setState function
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count:0
+      }
+    }
+
   render() {
     return (
-      <div>
+      <div style={{ backgroundColor: "red"}}>
         <h1>Counter Class Component</h1>
+        <h3>Counter : {this.state.count}</h3>
+        
+        <button onClick={()=>{
+            this.setState({count:this.state.count +1 })
+        }}>Add</button>
       </div>
     )
   }
