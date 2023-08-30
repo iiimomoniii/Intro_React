17e1a10cd3fe89e0c826a42bdd3757cfbae1964e
iiimomoniii <natdanai_thaiyala@live.com>
2023-08-30 12:24:24 +0700

    create image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Ex1Image from './components/ex1_image';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Ex1Image/>
     </div>
   );
 }
---
+++ src/components/ex1_image.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ex1_image() {
+  return (
+    <div>
+      <img src="/logo512.png" height="100" />
+      <img src="/images/giphy.gif" height="200" />
+    </div>
+  );
+}
