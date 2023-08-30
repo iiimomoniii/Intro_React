39f93493f04b97b70b417d58dc1a93b6e07fe5a7
iiimomoniii <natdanai_thaiyala@live.com>
2023-08-30 13:29:50 +0700

    show image by props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Ex1Image/>
+      <Ex1Image image="/images/giphy.gif"/>
     </div>
   );
 }
--- src/components/ex1_image.js
+++ src/components/ex1_image.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function ex1_image() {
+export default function ex1_image(props) {
   return (
     <div>
       <img src="/logo512.png" height="100" />
       <img src="/images/giphy.gif" height="200" />
+      {props.image && <img src={props.image} height="200" />}
     </div>
   );
 }
