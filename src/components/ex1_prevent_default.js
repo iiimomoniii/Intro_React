d6b0bc3ae456ff967ff3f06bed1bcd4c0f9cb02d
Natdanai <natdanai_thaiyala@live.com>
2023-09-01 17:14:44 +0700

    add comment

--- src/components/ex1_prevent_default.js
+++ src/components/ex1_prevent_default.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function ex1_prevent_default({handleSubmit}) {
   return (
     <div >
+    {/*block reload after close popup*/}
     <form onSubmit={event => {event.preventDefault();handleSubmit();}}>
       <label htmlFor='username'>Username</label>
       <input type='text' name='username' id='username'/>
