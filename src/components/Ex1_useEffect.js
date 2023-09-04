20973efd5142ced698817441f647212650110a12
Natdanai <natdanai_thaiyala@live.com>
2023-09-04 18:37:15 +0700

    use useEffect for update and init value

---
+++ src/components/Ex1_useEffect.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Ex1_useEffect() {
+
+    const [count, setCount] = useState(0);
+
+  {/*re-load component when need to update value using effect */}
+  useEffect(() =>{
+    // Called whenever state is changed
+    document.title = "Current count : " + count
+    // return () => {
+
+    // };
+  }, [count])
+  return (
+
+    <div>
+        <h1>{count}</h1>
+        <button onClick={()=>{setCount(count+1)}}>Add</button>
+    </div>
+  )
+}
