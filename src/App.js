80b1cfb7f26227d4d09a824c610a8d75f0bfe587
Natdanai <natdanai_thaiyala@live.com>
2023-09-05 14:28:36 +0700

    add action for prove destroy value

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 import Ex1_useEffect from './components/Ex1_useEffect';
-import Ex2_useEffectDestroyListener from './components/Ex1_useEffect';
+import Ex2_useEffectDestroyListener from './components/Ex2_useEffectDestroyListener';
 function App() {
+
+  const [isShow , setIsShow] = useState(true);
+
   return (
     <div className="App">
        <Ex1_useEffect/>
-       <Ex2_useEffectDestroyListener/>
+      {isShow && <Ex2_useEffectDestroyListener/>} 
+     <button onClick={()=>setIsShow(false)}>Hide</button>
     </div>
   );
 }
